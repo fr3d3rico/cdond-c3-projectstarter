fc3470ca97552cda779dfb03e74e848aeee33a07
Fred <frederico.a.soares@gmail.com>
2021-08-05 20:16:50 -0300

    Test after removing cache on cloudfront.

--- frontend/src/app/containers/Employee/components/Employees/index.tsx
+++ frontend/src/app/containers/Employee/components/Employees/index.tsx
@@ -205,7 +205,7 @@ export class Employees extends React.Component<
         <SearchBar searchInput={generalSearch} />
         <div className={`${style['g-sectionbar']} ${style['grid-x']}`}>
           <Breadcrumb
-            rootPathName='View & Manage Employees Fred 2'
+            rootPathName='View & Manage Employees'
             isSecondaryPage={false}
           >
             <li>
